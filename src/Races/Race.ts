13d6523cd7fde34d78da2d21ccac7d18d57f8b84
leandrochs <leandrochagask@gmail.com>
2022-05-31 12:56:29 -0300

    feat: cria a classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  protected _name: string;
+  protected _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+}
