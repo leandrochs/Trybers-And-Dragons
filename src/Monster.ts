85d320026fa747d8c2ad18b0a24fdde50eae49b9
leandrochs <leandrochagask@gmail.com>
2022-05-31 20:25:22 -0300

    feat: adiciona classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,30 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this.lifePoints - attackPoints;
+    if (damage > 0) this._lifePoints -= attackPoints;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
